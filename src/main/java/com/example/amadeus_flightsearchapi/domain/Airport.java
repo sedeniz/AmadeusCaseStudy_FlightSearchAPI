ba11839181365c8794c41cefc5fe1348430bdf6c
Seden Deniz Taşkın <sedendeniz@gmail.com>
2024-02-04 00:04:42 +0300

    Added Airport Entity

---
+++ src/main/java/com/example/amadeus_flightsearchapi/domain/Airport.java
@@ -0,0 +1,38 @@
+package com.example.amadeus_flightsearchapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String city;
+
+    public Airport() {}
+
+    public Airport(String city) {
+        this.city = city;
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
