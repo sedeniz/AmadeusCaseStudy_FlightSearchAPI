46e2ec4bc16c27c92aff5deeffba5259c85b759a
Seden Deniz Taşkın <sedendeniz@gmail.com>
2024-02-03 23:31:33 +0300

    Initial commit

---
+++ src/main/java/com/example/amadeus_flightsearchapi/AmadeusFlightSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.amadeus_flightsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusFlightSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmadeusFlightSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amadeus_flightsearchapi/AmadeusFlightSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amadeus_flightsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusFlightSearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
